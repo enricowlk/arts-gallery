81c4e522bc5d00495d4a0b93ad896f2524aa7727
enricowlk <enrico.woelck@gmail.com>
2025-06-01 16:12:14 +0200

    .

--- index.php
+++ index.php
@@ -8,7 +8,6 @@ session_start();
     <meta charset="UTF-8">
     <title>Art Gallery - Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Achtung: styles.css muss im public/assets/ Ordner liegen! -->
     <link rel="stylesheet" href="/public/assets/styles.css">
 </head>
 <body>
